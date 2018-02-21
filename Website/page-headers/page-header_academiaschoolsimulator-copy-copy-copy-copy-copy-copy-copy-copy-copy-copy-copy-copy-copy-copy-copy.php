b4a617052d74bed2f7b3e083c1e44a8ca6274420
Jehseh <jc.santiago0312@gmail.com>
2018-02-21 21:47:00 +0800

    added pages and page headers

---
+++ Website/page-headers/page-header_academiaschoolsimulator-copy-copy-copy-copy-copy-copy-copy-copy-copy-copy-copy-copy-copy-copy-copy.php
@@ -0,0 +1,86 @@
+<h1 class="h3name">Warrior Defense</h1>
+<div class="panel panel-default">
+
+</div>
+    <div class="panel-header">
+        <div class="non-phone">
+            <div class="row">
+                <div class="col-sm-9 ">
+                <img style="width:100%;" src="../testphotos/green_background_2-wallpaper-1920x1080.jpg" alt="Los Angeles">
+                </div>
+                <div class="col-sm-3">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img style="width:100%;" src="../testphotos/kadali_nikon_d5100_sample_photo-wallpaper-1920x1080.jpg" alt="Chicago">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img style="width:100%;" src="../testphotos/sample_brews-wallpaper-1920x1080.jpg" alt="New York">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img style="width:100%;" src="../testphotos/tree_flower-wallpaper-1920x1080.jpg" alt="New York">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="phone">
+            <div id="photoCarousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                      <img src="../testphotos/green_background_2-wallpaper-1920x1080.jpg" alt="Los Angeles">
+                    </div>
+
+                    <div class="item">
+                      <img src="../testphotos/kadali_nikon_d5100_sample_photo-wallpaper-1920x1080.jpg" alt="Chicago">
+                    </div>
+
+                    <div class="item">
+                      <img src="../testphotos/sample_brews-wallpaper-1920x1080.jpg" alt="New York">
+                    </div>
+
+                    <div class="item">
+                      <img src="../testphotos/tree_flower-wallpaper-1920x1080.jpg" alt="New York">
+                    </div>
+                </div>
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+    <div class="panel-body">
+        <h4>Price: FREE</h4>
+        <h5>Developed by:
+        <a href="../devs/coffeebrain.html">Coffee Brain Games</a>
+        </h5>
+        <h5>Released: Jan 2, 2014</h5>
+        <h5>Platform: iOS, Android, PC</h5>
+        <p>Developed by Marnielle Estrada and his team under the name 'Coffee Brain Games', Warrior Defense
+            was one of the first few games that was released worldwide through the Google Play Store.
+        </p>
+        <p>Warrior Defense is a spin-off the "Tower Defense" genre where you buy towers and place towers
+            in pre-existing areas for your towers, but in Warrior Defense you buy units and place them
+            on the ground. Those units can move around to help in defending while you use your spells.
+        </p>
+    </div>
+    <div class="panel-footer"><h4>Get it on</h4>
+<img src="../assets/img/badges/gp-badge.png" alt="Get it on Google Play"> </div>
+</div>
+
+
