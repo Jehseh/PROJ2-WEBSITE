63b43ea9dac3465bdf93e00abb700ca2e56606ca
Jehseh <jc.santiago0312@gmail.com>
2018-02-21 21:40:42 +0800

    added test photos

--- Website/page-headers/page-header_academiaschoolsimulator.php
+++ Website/page-headers/page-header_academiaschoolsimulator.php
@@ -1,6 +1,68 @@
 <h1 class="h3name">Warrior Defense</h1>
 <div class="panel panel-default">
-    <div class="panel-header"><div class="mainphoto">TESTER</div></div>
+
+</div>
+    <div class="panel-header">
+        <div class="non-phone">
+            <div class="row">
+                <div class="col-sm-9 ">
+                <img style="width:100%;" src="../testphotos/green_background_2-wallpaper-1920x1080.jpg" alt="Los Angeles">
+                </div>
+                <div class="col-sm-3">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img style="width:100%;" src="../testphotos/kadali_nikon_d5100_sample_photo-wallpaper-1920x1080.jpg" alt="Chicago">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img style="width:100%;" src="../testphotos/sample_brews-wallpaper-1920x1080.jpg" alt="New York">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img style="width:100%;" src="../testphotos/tree_flower-wallpaper-1920x1080.jpg" alt="New York">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="phone">
+            <div id="photoCarousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                      <img src="../testphotos/green_background_2-wallpaper-1920x1080.jpg" alt="Los Angeles">
+                    </div>
+
+                    <div class="item">
+                      <img src="../testphotos/kadali_nikon_d5100_sample_photo-wallpaper-1920x1080.jpg" alt="Chicago">
+                    </div>
+
+                    <div class="item">
+                      <img src="../testphotos/sample_brews-wallpaper-1920x1080.jpg" alt="New York">
+                    </div>
+
+                    <div class="item">
+                      <img src="../testphotos/tree_flower-wallpaper-1920x1080.jpg" alt="New York">
+                    </div>
+                </div>
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
     
     <div class="panel-body">
         <h4>Price: FREE</h4>
